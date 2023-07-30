104a87d5e4a6feea36d1882abe3b1e8c0f0723d6
Supriyo Saha <mm.saha1982@gmail.com>
2023-07-30 21:58:49 +0530

    side bar implemented

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,8 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
   return (
     <div>
       <p>Dash board page(protected)</p>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
